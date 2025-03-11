473819664a525d28723554ac081eed1da7e7575c
Leozzz999 <leozzzgimhan@gmail.com>
2025-03-11 15:59:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,4 +6,8 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID,
+ALIVE_IMG: process.env.ALIVE_IMG || "https://avatars.githubusercontent. com/u/106251140?v=4",
+ALIVE_MSG: process.env.ALIVE_MSG || "Helo, I am Yourname i am alive now!!",
 };
+
+
